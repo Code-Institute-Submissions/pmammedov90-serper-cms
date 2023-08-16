f3a4d7223688f75fd4b89b1f8c32cf079e8fa480
pmammedov90 <>
2023-08-16 23:43:00 +0000

    Define the register class

--- run.py
+++ run.py
@@ -44,3 +44,93 @@ def viewCourse(courses):
                 print("Please enter a valid course number.")
         else:
             print("Please enter a valid number or -1 to go back to the main menu")
+
+# Definition of the pupil class
+class pupil:
+    def __init__(self):
+        self.name = ""
+        self.id = ""
+        self.course = ""
+        self.grades = []
+
+# Definition of the register class
+class register:
+    def __init__(self, courses):
+        self.pupils = []
+        self.course_keys = [x['key'] for x in courses]
+
+    def register_pupils(self):
+        p = pupil()
+        p.id = input("Enter ID: ")
+        p.name = input("Enter Name: ")
+        print("\n--- Courses Available ---")  # Display the "Courses Available" title
+        for idx, k in enumerate(self.course_keys, start=1):
+            print(f"{idx}: {k}")
+
+        try:
+            num = int(input("Enter Course number: "))
+            if 1 <= num <= len(self.course_keys):
+                p.course = self.course_keys[num - 1]
+                print("\n------------- Registered\n")
+                self.pupils.append(p)
+            else:
+                print("Enter a valid course number. Registration failed.")
+        except ValueError:
+            print("Invalid input. Registration failed.")
+
+    # Method to grade a pupil
+    def grade_pupil(self):
+        pupil_id = input("Enter the ID of the pupil to grade: ")
+        found_pupil = None
+        for p in self.pupils:
+            if p.id == pupil_id:
+                found_pupil = p
+                break
+        if found_pupil is None:
+            print("Pupil not found.")
+            return
+
+        print("Enter grades (0-100) for the pupil:")
+        for x in range(1, 9):
+            try:
+                grade = int(input(f"Enter grade for unit no {x}: "))
+                if 0 <= grade <= 100:
+                    found_pupil.grades.append(grade)  # Append grades to existing list
+                else:
+                    print("Invalid grade. Please enter a grade between 0 and 100.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+        print("Grades updated.")
+
+    # Method to display pupil progress
+    def display_grades(self):
+        for p in self.pupils:
+            sum = 0
+            res = ""
+            for x in p.grades:
+                sum += int(x)
+            sum = sum / 8
+            if sum > 79:
+                res = "Inspiring"
+            elif sum > 60:
+                res = "Distinction"
+            elif sum > 50:
+                res = "Merit"
+            elif sum > 40:
+                res = "Pass"
+            else:
+                res = "Fail"
+
+            print("\n---------------\n")
+            print(f"Name: {p.name}")  # Display the pupil's name
+            print(f"ID: {p.id}")      # Display the pupil's provided ID
+            print(f"Average Grade: {sum}")
+            print(f"Result: {res}\n")
+
+            # Display grades with unit numbers
+            print("Grades for each unit:")
+            for unit_num, grade in enumerate(p.grades, start=1):
+                print(f"Unit {unit_num}: {grade}")
+
+
